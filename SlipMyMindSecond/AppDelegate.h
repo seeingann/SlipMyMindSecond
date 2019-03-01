b974537d4b726b71e5a428314504e7aed20831b5
AnnLaw <736882918@qq.com>
2019-03-01 11:16:54 +0800

    Initial Commit

---
+++ SlipMyMindSecond/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SlipMyMindSecond
+//
+//  Created by tangtang on 2019/3/1.
+//  Copyright © 2019年 huren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SlipMyMindSecond/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SlipMyMindSecond
+//
+//  Created by tangtang on 2019/3/1.
+//  Copyright © 2019年 huren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
